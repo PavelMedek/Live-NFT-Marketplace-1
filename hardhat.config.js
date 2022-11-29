f63bbb2fc022535aeec37f800bf523fd935449b7
Pavel Medek <85835897+PavelMedek@users.noreply.github.com>
2022-11-29 10:17:39 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,8 +8,9 @@ module.exports = {
 
     polygon_mumbai: {
       // chainId: 80001,
-      url: process.env.POLYGON_MUMBAI,
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
-    },
+      url: "https://polygon-mumbai.g.alchemy.com/v2/wL1maDWm41DH9mKb0zgKyDNSzykduI4T",
+      accounts: [
+        `fb80d9e3bf654405fce8b28c7b6eb8ba8708efe3d5057e8dceb5fb525f7a4d4c`,
+      ],
   },
 };
