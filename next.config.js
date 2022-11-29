aa2e2aa24c1711a25c109976adeb1b7804299ed0
Pavel Medek <85835897+PavelMedek@users.noreply.github.com>
2022-11-29 10:36:53 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["daulat-nft-marketplace.infura-ipfs.io", "infura-ipfs.io"],
+    domains: ["pavel-medek-nft.infura-ipfs.io", "infura-ipfs.io"],
   },
 };
 
